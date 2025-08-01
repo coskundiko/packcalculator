d4c3871d16f53172246cdcdba3eefa4657c0e835
Diko <coskundiko@gmail.com>
2025-08-01 10:13:34 +0200

    nm

--- app/handler/order/order.go
+++ app/handler/order/order.go
@@ -3,32 +3,60 @@ package order
 import (
 	"github.com/labstack/echo/v4"
 	"net/http"
-	"packcalculator/pkg/calculator"
+	"sync"
 )
 
-type OrderHandler struct{}
+type OrderHandler struct {
+	PackSizes []int `json:"pack_sizes"`
+	sync.Mutex
+}
 
-type CalculatePacksRequest struct {
-	OrderSize int   `json:"order_size"`
+type SetPackSizesRequest struct {
 	PackSizes []int `json:"pack_sizes"`
 }
 
+type CalculatePacksRequest struct {
+	OrderSize int `json:"order_size"`
+}
+
 func New() OrderHandler {
-	return OrderHandler{}
+	return OrderHandler{
+		PackSizes: []int{250, 500, 1000, 2000, 5000},
+	}
 }
 
 func (h *OrderHandler) CalculatePacks(c echo.Context) error {
 	req := CalculatePacksRequest{}
-
 	if err := c.Bind(req); err != nil {
 		return c.JSON(http.StatusBadRequest, "Invalid request body")
-	} else if req.OrderSize <= 0 {
+	}
+
+	if req.OrderSize <= 0 {
 		return c.JSON(http.StatusBadRequest, "order_size must be a positive number")
-	} else if len(req.PackSizes) == 0 {
+	}
+
+	//packs := calculator.CalculatePacks(req.OrderSize, req.PackSizes)
+
+	return c.JSON(http.StatusOK, h.PackSizes)
+}
+
+func (h *OrderHandler) GetPackSizes(c echo.Context) error {
+	return c.JSON(http.StatusOK, h.PackSizes)
+}
+
+func (h *OrderHandler) SetPackSizes(c echo.Context) error {
+	req := new(SetPackSizesRequest)
+	if err := c.Bind(req); err != nil {
+		return c.JSON(http.StatusBadRequest, "Invalid request body")
+	}
+
+	if len(req.PackSizes) == 0 {
 		return c.JSON(http.StatusBadRequest, "pack_sizes must be a positive number")
 	}
 
-	packs := calculator.CalculatePacks(req.OrderSize, req.PackSizes)
+	h.Mutex.Lock()
+	h.PackSizes = req.PackSizes
+	h.Mutex.Unlock()
 
-	return c.JSON(http.StatusOK, packs)
+	return c.JSON(http.StatusOK, h.PackSizes)
 }
--- server/routes.go
+++ server/routes.go
@@ -5,5 +5,7 @@ func (x *Server) API() {
 	x.Echo.Static("/", "./public")
 
 	api := x.Echo.Group("/api")
-	api.POST("/calculate-packs", x.Handler.Order.CalculatePacks).Name = "get_calculate_packs"
+	api.POST("/pack-sizes", x.Handler.Order.SetPackSizes).Name = "post_pack_sizes"
+	api.GET("/pack-sizes", x.Handler.Order.GetPackSizes).Name = "post_pack_sizes"
+	api.GET("/calculate-packs", x.Handler.Order.CalculatePacks).Name = "get_calculate_packs"
 }
