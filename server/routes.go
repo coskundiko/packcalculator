8737e686b8f43bc65eb426b880afab0a7d2a45af
Diko <coskundiko@gmail.com>
2025-08-01 08:35:00 +0200

    nm

--- server/routes.go
+++ server/routes.go
@@ -2,13 +2,8 @@ package server
 
 // We add and load the routes
 func (x *Server) API() {
-	x.Echo.Static("/store", "store").Name = "static_store"
 	x.Echo.Static("/", "./public")
-	//x.Echo.GET("/", func(c echo.Context) error {
-	//	return c.JSON(http.StatusOK, "OK - Mode: ")
-	//})
 
-	x.Echo.GET("/calculate-packs", x.Handler.Order.CalculatePacks).Name = "get_calculate_packs"
-
-	//api := e.Group("/api")
+	api := x.Echo.Group("/api")
+	api.POST("/calculate-packs", x.Handler.Order.CalculatePacks).Name = "get_calculate_packs"
 }
