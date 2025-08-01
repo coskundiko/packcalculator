100577e71d880efdb3f3e43d39ee900f3c8f8bd2
Diko <coskundiko@gmail.com>
2025-08-01 08:49:40 +0200

    nm

--- app/handler/order/order.go
+++ app/handler/order/order.go
@@ -3,15 +3,32 @@ package order
 import (
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"packcalculator/pkg/calculator"
 )
 
 type OrderHandler struct{}
+
+type CalculatePacksRequest struct {
+	OrderSize int   `json:"order_size"`
+	PackSizes []int `json:"pack_sizes"`
+}
 
 func New() OrderHandler {
 	return OrderHandler{}
 }
 
 func (h *OrderHandler) CalculatePacks(c echo.Context) error {
+	req := CalculatePacksRequest{}
 
-	return c.JSON(http.StatusOK, "ok")
+	if err := c.Bind(req); err != nil {
+		return c.JSON(http.StatusBadRequest, "Invalid request body")
+	} else if req.OrderSize <= 0 {
+		return c.JSON(http.StatusBadRequest, "order_size must be a positive number")
+	} else if len(req.PackSizes) == 0 {
+		return c.JSON(http.StatusBadRequest, "order_size must be a positive number")
+	}
+
+	packs := calculator.CalculatePacks(req.OrderSize, req.PackSizes)
+
+	return c.JSON(http.StatusOK, packs)
 }
